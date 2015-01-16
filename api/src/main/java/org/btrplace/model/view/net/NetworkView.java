a31576ff0459946ffd91cddacb016b94da628b6e
Vincent KHERBACHE <vincent.kherbache@inria.fr>
2015-01-16 19:22:53 +0100

    added method to write dot file from NetworkView

--- api/src/main/java/org/btrplace/model/view/net/NetworkView.java
+++ api/src/main/java/org/btrplace/model/view/net/NetworkView.java
@@ -4,8 +4,14 @@ import org.btrplace.model.Node;
 import org.btrplace.model.VM;
 import org.btrplace.model.view.ModelView;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by vkherbac on 08/12/14.
@@ -83,6 +89,99 @@ public class NetworkView implements ModelView, Cloneable {
         }
         return null;
     }
+
+    public List<Node> getConnectedNodes() {
+        List<Node> nodes = new ArrayList<>();
+        for (Switch sw : switches) {
+            for (Port p : sw.getPorts()) {
+                if (p.getRemote().getHost() instanceof Node) {
+                    if (!nodes.contains(p.getRemote().getHost())) nodes.add((Node) p.getRemote().getHost());
+                }
+            }
+        }
+        return nodes;
+    }
+
+    public boolean generateDot(String out, boolean fromLeftToRight) {
+
+        int numLink = 0;
+        List<Node> nodes = getConnectedNodes();
+        Set<Port> drawedLinks = new HashSet<>();
+
+        try {
+            BufferedWriter dot = new BufferedWriter(new FileWriter(out));
+            dot.append("digraph G {\n");
+            if (fromLeftToRight) { dot.append("rankdir=LR;\n"); }
+            // Draw nodes
+            for (Node n : nodes) {
+                dot.append("node").
+                        append(String.valueOf(n.id())).
+                        append(" [shape=box, color=green, label=\"model.Node ").
+                        append(String.valueOf(n.id())).
+                        append("\"];\n");
+            }
+            // Draw switches
+            for (Switch s : switches) {
+                numLink = 0;
+                dot.append("switch").
+                        append(String.valueOf(s.id())).
+                        append(" [shape=record, color=blue, label=\"model.Switch ").
+                        append(String.valueOf(s.id())).append("\\n[").
+                        append(bitsToString(s.getCapacity())).
+                        append("/s]");
+                for (Port p : s.getPorts()) {
+                    numLink++;
+                    dot.append("|<p").append(String.valueOf(numLink)).append(">/").append(String.valueOf(numLink));
+                }
+                dot.append("\"];\n");
+            }
+            // Draw links
+            for (Switch s : switches) {
+                numLink = 0;
+                for (Port p : s.getPorts()) {
+                    numLink++;
+                    if (!drawedLinks.contains(p) && !drawedLinks.contains(p.getRemote())) {
+                        dot.append("switch").
+                                append(String.valueOf(s.id())).
+                                append(":p").
+                                append(String.valueOf(s.getPorts().indexOf(p) + 1)).
+                                append(" -> ");
+
+                        if (p.getRemote().getHost() instanceof Node) {
+                            dot.append("node").append(String.valueOf(((Node) p.getRemote().getHost()).id()));
+                        }
+                        else {
+                            dot.append("switch").
+                                    append(String.valueOf(((Switch) p.getRemote().getHost()).id())).
+                                    append(":p").
+                                    append(String.valueOf(((Switch) p.getRemote().getHost()).getPorts().
+                                            indexOf(p.getRemote()) + 1));
+                        }
+                        dot.append(" [arrowhead=none, color=red, label=\"").
+                                append(bitsToString(Math.min(p.getBandwidth(), p.getRemote().getBandwidth()))).
+                                append("/s\"]\n");
+                        drawedLinks.add(p);
+                        drawedLinks.add(p.getRemote());
+                    }
+                }
+            }
+            dot.append("}\n");
+            dot.flush();
+            dot.close();
+        } catch (IOException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private String bitsToString(long megabits) {
+        int unit = 1000;
+        if (megabits < unit) return megabits + " mb";
+        int exp = (int) (Math.log(megabits) / Math.log(unit));
+        return new DecimalFormat("#.##").format(megabits / Math.pow(unit, exp)) + "GTPE".charAt(exp-1) + "b";
+    }
+
 
     @Override
     public String getIdentifier() {
