1196d175ce71946c027f0f7f4d3d673e36d35b3c
Vincent KHERBACHE <vincent@kherbache.fr>
2015-04-15 00:19:58 +0200

    socc tests: added 100gb test

--- choco/src/test/java/org/btrplace/scheduler/choco/view/net/nancy/macro/SoccTest.java
+++ choco/src/test/java/org/btrplace/scheduler/choco/view/net/nancy/macro/SoccTest.java
@@ -131,7 +131,7 @@ public class SoccTest {
         for (Node n : srcNodes) { energyView.setConsumption(n, powerIdleNode); }
         for (Node n : dstNodes) { energyView.setConsumption(n, powerIdleNode); }
         for (VM vm : vms) { energyView.setConsumption(vm, powerVM); }
-        mo.attach(energyView);
+        //mo.attach(energyView);
 
         // Add a NetworkView view
         NetworkView net = new NetworkView();
@@ -309,7 +309,7 @@ public class SoccTest {
         for (Node n : srcNodes) { energyView.setConsumption(n, powerIdleNode); }
         for (Node n : dstNodes) { energyView.setConsumption(n, powerIdleNode); }
         for (VM vm : vms) { energyView.setConsumption(vm, powerVM); }
-        mo.attach(energyView);
+        //mo.attach(energyView);
 
         // Add a NetworkView view
         NetworkView net = new NetworkView();
@@ -492,7 +492,7 @@ public class SoccTest {
         for (Node n : srcNodes) { energyView.setConsumption(n, powerIdleNode); }
         for (Node n : dstNodes) { energyView.setConsumption(n, powerIdleNode); }
         for (VM vm : vms) { energyView.setConsumption(vm, powerVM); }
-        mo.attach(energyView);
+        //mo.attach(energyView);
 
         // Add a NetworkView view
         NetworkView net = new NetworkView();
@@ -523,6 +523,244 @@ public class SoccTest {
         swDstRack4.connect(1000, dstNodes.subList(nbNodesRack*3,nbNodesRack*4));
         swMain.connect(40000, swSrcRack1, swSrcRack2, swSrcRack3, swSrcRack4, swSrcRack5, swSrcRack6, swSrcRack7, swSrcRack8,
                 swDstRack1, swDstRack2, swDstRack3, swDstRack4);
+        mo.attach(net);
+        net.generateDot(path + "topology.dot", false);
+
+        // Set parameters
+        DefaultParameters ps = new DefaultParameters();
+        ps.setVerbosity(2);
+        ps.setTimeLimit(60);
+        //ps.setMaxEnd(600);
+        ps.doOptimize(false);
+
+        // Set the custom migration transition
+        ps.getTransitionFactory().remove(ps.getTransitionFactory().getBuilder(VMState.RUNNING, VMState.RUNNING));
+        ps.getTransitionFactory().add(new MigrateVMTransition.Builder());
+        // Register custom objectives
+        ps.getConstraintMapper().register(new CMinMTTRObjective.Builder());
+        ps.getConstraintMapper().register(new CMinEnergyObjective.Builder());
+        // Register new constraints
+        ps.getConstraintMapper().register(new CPowerBudget.Builder());
+
+        // Migrate all VMs to destination nodes
+        List<SatConstraint> cstrs = new ArrayList<>();
+        int vm_num = 0;
+        for (int i=0; i<nbDstNodes; i++) {
+            cstrs.add(new Fence(vms.get(vm_num), Collections.singleton(dstNodes.get(i))));
+            cstrs.add(new Fence(vms.get(vm_num+1), Collections.singleton(dstNodes.get(i))));
+            cstrs.add(new Fence(vms.get(nbVMs-1-vm_num), Collections.singleton(dstNodes.get(i))));
+            cstrs.add(new Fence(vms.get(nbVMs-2-vm_num), Collections.singleton(dstNodes.get(i))));
+            vm_num+=2;
+        }
+
+        // Shutdown source nodes
+        for (Node n : srcNodes) { cstrs.add(new Offline(n)); }
+
+        // Add continuous power budgets
+        //cstrs.add(new PowerBudget(0, 120, 8400));
+
+        // Set a custom objective
+        DefaultChocoScheduler sc = new DefaultChocoScheduler(ps);
+        //Instance i = new Instance(mo, cstrs, new MinMTTR());
+        Instance i = new Instance(mo, cstrs, new MinMTTRObjective());
+        //Instance i = new Instance(mo, cstrs,  new MinEnergyObjective());
+
+        ReconfigurationPlan p;
+        try {
+            p = sc.solve(i);
+            Assert.assertNotNull(p);
+
+            ReconfigurationPlanConverter planConverter = new ReconfigurationPlanConverter();
+            JSONObject obj = null;
+            try {
+                obj =  planConverter.toJSON(p);
+            } catch (JSONConverterException e) {
+                System.err.println("Error while converting plan: " + e.toString());
+                //e.printStackTrace();
+            }
+            try {
+                FileWriter file = new FileWriter(path + "socc.json");
+                file.write(obj.toJSONString());
+                file.flush();
+                file.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ActionsToCSV.convert(p.getActions(), path + "actions.csv");
+            energyView.plotConsumption(p, path + "energy.csv");
+            System.err.println(p);
+            System.err.flush();
+
+        } finally {
+            System.err.println(sc.getStatistics());
+        }
+    }
+
+
+    @Test
+    public void decommissioning_100gb() throws SchedulerException,ContradictionException {
+
+        String path = new File("").getAbsolutePath() +
+                "/choco/src/test/java/org/btrplace/scheduler/choco/view/net/nancy/macro/";
+
+        // Set nb of nodes and vms
+        int nbNodesRack = 24;
+        int nbSrcNodes = nbNodesRack * 20;
+        int nbDstNodes = nbNodesRack * 10;
+        int nbVMs = nbSrcNodes * 2;
+        //int nbVMsOnDestNodes = 4;
+
+        // Set mem + cpu for VMs and Nodes
+        int memVM = 4, cpuVM = 1;
+        int memSrcNode = 16, cpuSrcNode = 4;
+        int memDstNode = 16, cpuDstNode = 4;
+
+        // Set memoryUsed and dirtyRate (for all VMs)
+        int tpl1MemUsed = 2000, tpl1MaxDirtySize = 5, tpl1MaxDirtyDuration = 3; double tpl1DirtyRate = 0; // idle vm
+        int tpl2MemUsed = 4000, tpl2MaxDirtySize = 96, tpl2MaxDirtyDuration = 2; double tpl2DirtyRate = 3; // stress --vm 1000 --bytes 70K
+        int tpl3MemUsed = 2000, tpl3MaxDirtySize = 96, tpl3MaxDirtyDuration = 2; double tpl3DirtyRate = 3; // stress --vm 1000 --bytes 70K
+        int tpl4MemUsed = 4000, tpl4MaxDirtySize = 5, tpl4MaxDirtyDuration = 3; double tpl4DirtyRate = 0; // idle vm
+
+        // Define power values
+        int powerIdleNode = 110, powerVM = 16, powerBoot = 20;
+        int maxConsumption = (nbSrcNodes*powerIdleNode)+(nbDstNodes*powerIdleNode)+(powerVM*nbVMs)+5000;
+        //maxConsumption = 9000;
+        //maxConsumption = 9200;
+
+        // New default model
+        Model mo = new DefaultModel();
+        Mapping ma = mo.getMapping();
+
+        // Create online source nodes and offline destination nodes
+        List<Node> srcNodes = new ArrayList<>(), dstNodes = new ArrayList<>();
+        for (int i=0; i<nbSrcNodes; i++) { srcNodes.add(mo.newNode()); ma.addOnlineNode(srcNodes.get(i)); }
+        for (int i=0; i<nbDstNodes; i++) { dstNodes.add(mo.newNode()); ma.addOfflineNode(dstNodes.get(i)); }
+
+        // Set boot and shutdown time
+        for (Node n : dstNodes) { mo.getAttributes().put(n, "boot", 120); /*~2 minutes to boot*/ }
+        for (Node n : srcNodes) {  mo.getAttributes().put(n, "shutdown", 17); /*~30 seconds to shutdown*/ }
+
+        // Create running VMs on src nodes
+        List<VM> vms = new ArrayList<>(); VM v;
+        for (int i=0; i<nbSrcNodes; i++) {
+            if (i%2 == 0) {
+                v = mo.newVM(); vms.add(v);
+                mo.getAttributes().put(v, "memUsed", tpl1MemUsed);
+                mo.getAttributes().put(v, "dirtyRate", tpl1DirtyRate);
+                mo.getAttributes().put(v, "maxDirtySize", tpl1MaxDirtySize);
+                mo.getAttributes().put(v, "maxDirtyDuration", tpl1MaxDirtyDuration);
+                ma.addRunningVM(v, srcNodes.get(i));
+                v = mo.newVM(); vms.add(v);
+                mo.getAttributes().put(v, "memUsed", tpl2MemUsed);
+                mo.getAttributes().put(v, "dirtyRate", tpl2DirtyRate);
+                mo.getAttributes().put(v, "maxDirtySize", tpl2MaxDirtySize);
+                mo.getAttributes().put(v, "maxDirtyDuration", tpl2MaxDirtyDuration);
+                ma.addRunningVM(v, srcNodes.get(i));
+            }
+            else {
+                v = mo.newVM(); vms.add(v);
+                mo.getAttributes().put(v, "memUsed", tpl3MemUsed);
+                mo.getAttributes().put(v, "dirtyRate", tpl3DirtyRate);
+                mo.getAttributes().put(v, "maxDirtySize", tpl3MaxDirtySize);
+                mo.getAttributes().put(v, "maxDirtyDuration", tpl3MaxDirtyDuration);
+                ma.addRunningVM(v, srcNodes.get(i));
+                v = mo.newVM(); vms.add(v);
+                mo.getAttributes().put(v, "memUsed", tpl4MemUsed);
+                mo.getAttributes().put(v, "dirtyRate", tpl4DirtyRate);
+                mo.getAttributes().put(v, "maxDirtySize", tpl4MaxDirtySize);
+                mo.getAttributes().put(v, "maxDirtyDuration", tpl4MaxDirtyDuration);
+                ma.addRunningVM(v, srcNodes.get(i));
+            }
+        }
+
+        // Add resource views
+        ShareableResource rcMem = new ShareableResource("mem", 0, 0);
+        ShareableResource rcCPU = new ShareableResource("cpu", 0, 0);
+        for (Node n : srcNodes) { rcMem.setCapacity(n, memSrcNode); rcCPU.setCapacity(n, cpuSrcNode); }
+        for (Node n : dstNodes) { rcMem.setCapacity(n, memDstNode); rcCPU.setCapacity(n, cpuDstNode); }
+        for (VM vm : vms) { rcMem.setConsumption(vm, memVM); rcCPU.setConsumption(vm, cpuVM); }
+        mo.attach(rcMem);
+        mo.attach(rcCPU);
+
+        // Add the EnergyView and set nodes & vms consumption
+        EnergyView energyView = new EnergyView(maxConsumption);
+        energyView.setBootOverhead(powerBoot); // % energy overhead during boot
+        for (Node n : srcNodes) { energyView.setConsumption(n, powerIdleNode); }
+        for (Node n : dstNodes) { energyView.setConsumption(n, powerIdleNode); }
+        for (VM vm : vms) { energyView.setConsumption(vm, powerVM); }
+        //mo.attach(energyView);
+
+        // Add a NetworkView view
+        NetworkView net = new NetworkView();
+        Switch swSrcRack1 = net.newSwitch();
+        Switch swSrcRack2 = net.newSwitch();
+        Switch swSrcRack3 = net.newSwitch();
+        Switch swSrcRack4 = net.newSwitch();
+        Switch swSrcRack5 = net.newSwitch();
+        Switch swSrcRack6 = net.newSwitch();
+        Switch swSrcRack7 = net.newSwitch();
+        Switch swSrcRack8 = net.newSwitch();
+        Switch swSrcRack9 = net.newSwitch();
+        Switch swSrcRack10 = net.newSwitch();
+        Switch swSrcRack11 = net.newSwitch();
+        Switch swSrcRack12 = net.newSwitch();
+        Switch swSrcRack13 = net.newSwitch();
+        Switch swSrcRack14 = net.newSwitch();
+        Switch swSrcRack15 = net.newSwitch();
+        Switch swSrcRack16 = net.newSwitch();
+        Switch swSrcRack17 = net.newSwitch();
+        Switch swSrcRack18 = net.newSwitch();
+        Switch swSrcRack19 = net.newSwitch();
+        Switch swSrcRack20 = net.newSwitch();
+        Switch swDstRack1 = net.newSwitch();
+        Switch swDstRack2 = net.newSwitch();
+        Switch swDstRack3 = net.newSwitch();
+        Switch swDstRack4 = net.newSwitch();
+        Switch swDstRack5 = net.newSwitch();
+        Switch swDstRack6 = net.newSwitch();
+        Switch swDstRack7 = net.newSwitch();
+        Switch swDstRack8 = net.newSwitch();
+        Switch swDstRack9 = net.newSwitch();
+        Switch swDstRack10 = net.newSwitch();
+        Switch swMain = net.newSwitch();
+        swSrcRack1.connect(1000, srcNodes.subList(0,nbNodesRack));
+        swSrcRack2.connect(1000, srcNodes.subList(nbNodesRack,nbNodesRack*2));
+        swSrcRack3.connect(1000, srcNodes.subList(nbNodesRack*2,nbNodesRack*3));
+        swSrcRack4.connect(1000, srcNodes.subList(nbNodesRack*3,nbNodesRack*4));
+        swSrcRack5.connect(1000, srcNodes.subList(nbNodesRack*4,nbNodesRack*5));
+        swSrcRack6.connect(1000, srcNodes.subList(nbNodesRack*5,nbNodesRack*6));
+        swSrcRack7.connect(1000, srcNodes.subList(nbNodesRack*6,nbNodesRack*7));
+        swSrcRack8.connect(1000, srcNodes.subList(nbNodesRack*7,nbNodesRack*8));
+        swSrcRack9.connect(1000, srcNodes.subList(nbNodesRack*8,nbNodesRack*9));
+        swSrcRack10.connect(1000, srcNodes.subList(nbNodesRack*9,nbNodesRack*10));
+        swSrcRack11.connect(1000, srcNodes.subList(nbNodesRack*10,nbNodesRack*11));
+        swSrcRack12.connect(1000, srcNodes.subList(nbNodesRack*11,nbNodesRack*12));
+        swSrcRack13.connect(1000, srcNodes.subList(nbNodesRack*12,nbNodesRack*13));
+        swSrcRack14.connect(1000, srcNodes.subList(nbNodesRack*13,nbNodesRack*14));
+        swSrcRack15.connect(1000, srcNodes.subList(nbNodesRack*14,nbNodesRack*15));
+        swSrcRack16.connect(1000, srcNodes.subList(nbNodesRack*15,nbNodesRack*16));
+        swSrcRack17.connect(1000, srcNodes.subList(nbNodesRack*16,nbNodesRack*17));
+        swSrcRack18.connect(1000, srcNodes.subList(nbNodesRack*17,nbNodesRack*18));
+        swSrcRack19.connect(1000, srcNodes.subList(nbNodesRack*18,nbNodesRack*19));
+        swSrcRack20.connect(1000, srcNodes.subList(nbNodesRack*19,nbNodesRack*20));
+        swDstRack1.connect(1000, dstNodes.subList(0,nbNodesRack));
+        swDstRack2.connect(1000, dstNodes.subList(nbNodesRack,nbNodesRack*2));
+        swDstRack3.connect(1000, dstNodes.subList(nbNodesRack*2,nbNodesRack*3));
+        swDstRack4.connect(1000, dstNodes.subList(nbNodesRack*3,nbNodesRack*4));
+        swDstRack5.connect(1000, dstNodes.subList(nbNodesRack*4,nbNodesRack*5));
+        swDstRack6.connect(1000, dstNodes.subList(nbNodesRack*5,nbNodesRack*6));
+        swDstRack7.connect(1000, dstNodes.subList(nbNodesRack*6,nbNodesRack*7));
+        swDstRack8.connect(1000, dstNodes.subList(nbNodesRack*7,nbNodesRack*8));
+        swDstRack9.connect(1000, dstNodes.subList(nbNodesRack*8,nbNodesRack*9));
+        swDstRack10.connect(1000, dstNodes.subList(nbNodesRack*9,nbNodesRack*10));
+        swMain.connect(100000,
+                swSrcRack1, swSrcRack2, swSrcRack3, swSrcRack4, swSrcRack5, swSrcRack6, swSrcRack7, swSrcRack8,
+                swSrcRack9, swSrcRack10, swSrcRack11, swSrcRack12, swSrcRack13, swSrcRack14, swSrcRack15, swSrcRack16,
+                swSrcRack17, swSrcRack8, swSrcRack19, swSrcRack20,
+                swDstRack1, swDstRack2, swDstRack3, swDstRack4, swDstRack5, swDstRack6, swDstRack7, swDstRack8,
+                swDstRack9, swDstRack10
+        );
         mo.attach(net);
         net.generateDot(path + "topology.dot", false);
 
